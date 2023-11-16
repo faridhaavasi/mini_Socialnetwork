cfc4089c7e6d46a07bd9267bd42059f14883ecb3
faridhaavasi <faridhavasi777@gmail.com>
2023-11-16 17:18:19 +0330

    use class based view View

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.HomeIndex.as_view(), name='home'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views import View
 from django.http import HttpResponse
 # Create your views here.
 
-def home(request):
-    return HttpResponse('hi dev')
+class HomeIndex(View):
+    def get(self, request):
+        return HttpResponse('hi cb')
