422d0e940d73b530cf0bd5276ac54bf113843bf9
faridhaavasi <faridhavasi777@gmail.com>
2023-11-24 13:51:04 +0330

    backendauthentication email add

--- core/settings.py
+++ core/settings.py
@@ -103,6 +103,13 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+#auth
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.auth.EmailBackend',
+]
+
+LOGIN_URL = '/account/login/'
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
