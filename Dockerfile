901af6affb05f549af58fa886fd4bff28fe7c5e2
faridhaavasi <faridhavasi777@gmail.com>
2024-01-16 23:55:30 +0330

    restart project

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python
-WORKDIR /src
-COPY requirments.txt /src
-EXPOSE 8000
-RUN pip install -r requirments.txt
-COPY . /src
-
-
--- home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views import View
-from django.http import HttpResponse
-# Create your views here.
-
-class HomeIndex(View):
-    def get(self, request):
-        return render(request, 'home/index.html', {'requust': request})
