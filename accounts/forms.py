3cb0cb5a2b2b8392806cb6bed936054a1688888f
faridhaavasi <faridhavasi777@gmail.com>
2024-01-18 01:42:52 +0330

    login 1.0.0

--- accounts/forms.py
+++ accounts/forms.py
@@ -13,3 +13,17 @@ class RegisterForm(forms.Form):
         attrs={'class': 'form-control',
                'placeholder': 'enter your password'})
     )
+
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control',
+        'placeholder': 'enter  username'})
+    )
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class': 'form-control',
+               'placeholder': 'enter your password'})
+    )
+
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib import messages
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 class RegisterView(View):
     form_class = RegisterForm
     template = 'accounts/register.html'
@@ -19,3 +20,19 @@ class RegisterView(View):
         return render(request, self.template, {'form': form})
 
 
+class LoginView(View):
+    form_class = LoginForm
+    template = 'accounts/login.html'
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(data=request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(User,username=cd['username'], password=cd['password'])
+            login(request, user)
+            messages.success(request, 'you are logined', 'success')
+            return redirect('home:home')
+        return render(request, self.template, {'form': form})
