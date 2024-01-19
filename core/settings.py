5a394886029c9b3eec61a0630598786bf5b390fc
faridhaavasi <faridhavasi777@gmail.com>
2024-01-19 14:15:54 +0330

    authentication backend add

--- core/settings.py
+++ core/settings.py
@@ -125,3 +125,9 @@ STATICFILES_DIRS  = (os.path.join(BASE_DIR, 'static'), )
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "accounts.authentication.EmaliAuthentication",
+]
+
