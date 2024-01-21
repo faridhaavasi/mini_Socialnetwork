e53652d3dd150165a3da2078bb27dae998d146ae
faridhaavasi <faridhavasi777@gmail.com>
2024-01-21 14:35:31 +0330

    add model post

--- home/models.py
+++ home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 # Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    slug = models.SlugField(max_length=20, blank=True, null=True)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        self.slug = slugify(self.title)
+        super().save()
+
+    def __str__(self):
+        return f'{self.title} - {self.user.username}'
