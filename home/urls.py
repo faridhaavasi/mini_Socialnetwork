1484bcd13e3e65febdec9c6564b84eba43418237
faridhaavasi <faridhavasi777@gmail.com>
2024-01-22 01:01:22 +0330

    addpost added

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ app_name = 'home'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('detailpost/<int:pk>/<slug:slug>', views.DetailPostView.as_view(), name='detailpost'),
+    path('addpost', views.AddPostUserView.as_view(), name='addpost'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import AddPostUserForm
+from django.contrib import messages
+
 class HomeView(View):
     def get(self, request):
         return render(request, 'home/index.html', {})
@@ -11,3 +14,23 @@ class DetailPostView(LoginRequiredMixin,View):
     def get(self, request, pk, slug):
         post = Post.objects.get(pk=pk, slug=slug)
         return render(request, 'home/detailpost.html', {'post': post})
+
+
+
+
+class AddPostUserView(LoginRequiredMixin, View):
+    form_class = AddPostUserForm
+    template = 'home/addpostuser.html'
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template, {'form': form})
+    def post(self, request):
+        form = self.form_class(data=request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            messages.success(request, 'added post', 'success')
+            return redirect('accounts:selfprofile')
+        return render(request, self.template, {'form': form})
+
