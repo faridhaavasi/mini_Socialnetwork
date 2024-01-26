ccbba82a512e360ed8034425247c8e94fcdce846
faridhaavasi <faridhavasi777@gmail.com>
2024-01-27 00:33:49 +0330

    add folowing model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class RelationInstance(models.Model):
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='to_user')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.following} - {self.to_user}'
