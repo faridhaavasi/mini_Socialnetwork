6b69f1834b60ed156ad32fcd9f44392c79534aa1
faridhaavasi <faridhavasi777@gmail.com>
2024-01-30 18:20:07 +0330

    add model comment

--- home/models.py
+++ home/models.py
@@ -23,3 +23,27 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.title} - {self.user.username}'
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'comments for post{self.post} by {self.user}'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
